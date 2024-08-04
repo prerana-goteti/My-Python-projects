326b319d5440d5360da630adc4754e4d1ef8b1ea
prerna <149256631+prerana-goteti@users.noreply.github.com>
2024-08-04 11:38:08 -0400

    list_tuples.py

--- list_tuples.py
+++ list_tuples.py
@@ -1,1 +1,51 @@
-list_tuples.py
+thislist = ["apple", "banana", "cherry"]
+print(thislist)
+['apple', 'banana', 'cherry']
+print(thislist[0])
+
+print(thislist[1])
+print(thislist[4])
+print(len(thislist))
+
+thislist = ["apple", "banana", "cherry",102, 2.0,"This is not apple"]
+print(thislist)
+
+thislist.append("July 4th")
+print(thislist)
+
+thislist.remove("banana")
+print(thislist)
+
+['apple', 'cherry', 102, 2.0, 'This is not apple', 'July 4th']
+thislist.pop(1)
+
+print(thislist)
+['apple', 102, 2.0, 'This is not apple', 'July 4th']
+
+MyNumberList=[1,343,232,2,34,6,3,2424,23]
+MyNumberList.sort()
+print(MyNumberList.sort())
+
+print(MyNumberList)
+[1, 2, 3, 6, 23, 34, 232, 343, 2424]
+MyNumberList=[1,343,232,2,34,6,3,2424,23]
+MyNumberList.sort()
+print(MyNumberList)
+
+MyStringList=["Apple","banana","grapes"]
+print(MyNumberList)
+
+[1, 2, 3, 6, 23, 34, 232, 343, 2424]
+MyNumberList.pop(5)
+
+print(MyNumberList)
+[1, 2, 3, 6, 23, 232, 343, 2424]
+MyStringList=["Apple","banana","grapes"]
+MyStringList.sort()
+print(MyStringList)
+
+MyStringList=["banana", "Apple","banana","grapes"]
+print(MyStringList)
+
+MyStringList.sort()
+print(MyStringList)
