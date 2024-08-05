722620fdc16ddd2d7e5678a864a64ff04c5aea55
prerna <149256631+prerana-goteti@users.noreply.github.com>
2024-08-04 20:12:17 -0400

    Add files via upload
    These are student records of 5 students, describing their names, student ids, grade, books they have checked out, the type of food they like, and their bus number. All of the personal information other than their names aren't real.

---
+++ python_collections.py
@@ -0,0 +1,49 @@
+student_record_1 = {
+    "Name" : "Prerana",
+    "Student Id" : "085830",
+    "Grade" : "8",
+    "Library Books" : "New Moon",
+    "Food Preferences" : "Pizza and Ice Cream",
+    "School Bus" : "#02"
+}
+print(student_record_1)
+
+student_record_2 = {
+    "Name" : "Akhil",
+    "Student Id" : "30946",
+    "Grade" : "11",
+    "Library Books" : "Zodiac Academy",
+    "Food Preferences" : "Tacos and Popcorn",
+    "School Bus" : "#19"
+}
+print(student_record_2)
+
+student_record_3 = {
+    "Name" : "Sairam",
+    "Student Id" : "30685",
+    "Grade" : "7",
+    "Library Books" : "Keeper of the Lost Cities",
+    "Food Preferences" : "Biriyani and Nachos",
+    "School Bus" : "#05"
+}
+print(student_record_3)
+
+student_record_4 = {
+    "Name" : "Diya",
+    "Student Id" : "31862",
+    "Grade" : "8",
+    "Library Books" : "The Hunger Games",
+    "Food Preferences" : "Pasta and Granola Bars",
+    "School Bus" : "#23"
+}
+print(student_record_4)
+
+student_record_5 = {
+    "Name" : "Allison",
+    "Student Id" : "39860",
+    "Grade" : "8",
+    "Library Books" : "Twisted Love",
+    "Food Preferences" : "Noodles and Ice Cream",
+    "School Bus" : "#01"
+}
+print(student_record_5)
